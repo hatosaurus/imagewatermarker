64a277688fdbd3ab379c7833e972292576fbc4a1
Jessica <94.jessicaschmidt@gmail.com>
2024-01-05 14:19:36 -0700

    Rebuild to function

--- main.py
+++ main.py
@@ -1,21 +1,44 @@
 from PIL import Image
+import os
 
-input_image_path = "raw_images/Sea_Dragon_.jpg"
+input_image_path = "raw_images/unnamed.jpg"
 watermark_image_path = "watermark/jessicaschmidt_watermark.png"
 output_image_path = "watermarked_images/BIRD_GREEN_WATERMARKED.jpg"
-
-base_image = Image.open(input_image_path) #open base image
-watermark = Image.open(watermark_image_path) #open water mark
-watermark = watermark.resize(base_image.size)
-
-width, height = base_image.size
-
-width_of_watermark, height_of_watermark = watermark.size
-position = (int(width/2-width_of_watermark/2), int(height/2-height_of_watermark/2))
+folder_dir = "raw_images"
 
 
-transparent = Image.new('RGBA', (width, height), (0, 0, 0, 0))
-transparent.paste(base_image, (0, 0))
-transparent.paste(watermark, position, mask=watermark)
-# Temporary name
-transparent.convert('RGB').save(output_image_path)
+def add_watermark(raw_image, watermark_image, output_path):
+    base_image = Image.open(raw_image)
+    watermark = Image.open(watermark_image)
+
+    # Get base parameters to apply to watermark
+    width, height = base_image.size
+
+    # Apply width for both parameters to keep watermark a square
+    watermark = watermark.resize((width, width))
+
+    # Find the new size of the watermark
+    width_of_watermark, height_of_watermark = watermark.size
+
+    # Centers the watermark on the image
+    position = (int(width / 2 - width_of_watermark / 2), int(height / 2 - height_of_watermark / 2))
+
+    # Create new image called transparent, it is the size of our base image
+    transparent = Image.new('RGBA', (width, height), (0, 0, 0, 0))
+
+    # Paste the base image in at a specific size
+    transparent.paste(base_image, (0, 0))
+
+    # Paste the watermark in at a specific size
+    transparent.paste(watermark, position, mask=watermark)
+
+    # Temporary name
+    transparent.convert('RGB').save(output_path)
+    print(f"Watermarking complete for {raw_image}.")
+    return
+
+
+for image in os.listdir(folder_dir):
+    image_path = f"raw_images/{image}"
+    output_path = f"watermarked_images/{image}"
+    add_watermark(image_path, watermark_image_path, output_path)
